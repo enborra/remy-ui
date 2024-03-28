c9195d28d5773172833e7cfe6240e7a90449b6c8
Andres Krogh-Walker <andres@Andress-MacBook-Pro.local>
2024-03-27 17:53:34 -0700

    .

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,8 +6,6 @@ import './Popup.css';
 
 class Popup extends React.Component {
 
-	
-
 	constructor(props){
 		super(props);
 
@@ -17,8 +15,6 @@ class Popup extends React.Component {
 	}
 
 	doThing(q){
-		console.log('asdf');
-		console.log(q);
 		this.isOpen = true;
 
 		var u = 'https://hackathon-2024-cmd-k-dc02368c323c.herokuapp.com/search';
@@ -34,12 +30,11 @@ class Popup extends React.Component {
 				this.setState({ resultContent: data.search_id })
 				this.search_id = data.search_id;
 
-				console.log(this.search_id);
+				this.isOpen = true;
 			})
 			.catch((error) => {
 				console.log(error);
-			})
-
+			});
 	}
 
 	doSearch(evt){
@@ -48,7 +43,7 @@ class Popup extends React.Component {
 		if( this.timeout ) clearTimeout(this.timeout);
 		this.timeout = setTimeout(() => {
 			this.doThing(evt.target.value);
-		}, 300);
+		}, 1000);
 	}
 
 	render(){
@@ -57,7 +52,7 @@ class Popup extends React.Component {
 				<div class="spacer"></div>
 				<input type="text" className="search-box" onChange={evt => this.doSearch(evt)}></input>
 
-				{this.isOpen && <ResultsPanel content={this.search_id}/>}
+				{this.isOpen && <ResultsPanel sid={this.search_id}/>}
 			</div>
 		)
 	}
--- src/components/ResultsPanel.js
+++ src/components/ResultsPanel.js
@@ -3,21 +3,98 @@ import React, { useEffect, useState } from "react";
 import './ResultsPanel.css';
 
 
-class ResultsPanel extends React.Component {
+export function ResultsPanel(props){
 
-  constructor(props){
-    super(props);
+  const [count, setCount] = useState(0);
+  const content = 'THISISTHESID';
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      poll();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  const poll = () => {
+    console.log('Result Panel polling!');
+
+    var u = 'https://hackathon-2024-cmd-k-dc02368c323c.herokuapp.com/search/'+props.sid;
+
+    fetch(u,{
+      method: 'GET',
+      headers: {'Content-Type': 'application/json' },
+      mode: 'cors'
+    })
+      .then(response => response.json())
+      .then(data => {
+        var resp = '';
+
+        if( data.state != 'completed' ){
+          resp = resp + data.state;  
+        }
+
+        if( data.results ){
+          if( data.results.length > 0 ){
+            resp = resp + ' ' + data.results[0].answer;
+          }
+        }
+
+        setCount(resp);
+
+        console.log('response from poll:');
+      });
   }
 
-  render(){
-    return (
+  // constructor(props){
+  //   super(props);
+
+  //   console.log('results panel:');
+  //   this.sid = this.props.content;
+
+  //   // this.startPolling();  
+  // }
+
+  // startPolling(sid){
+  //   this.sid = sid;
+  //   const timer = setInterval(this.pollForResults, 2000);
+
+  // }
+
+  // pollForResults(){
+  //   console.log( 'polling server for results..');
+
+  //   var u = 'https://hackathon-2024-cmd-k-dc02368c323c.herokuapp.com/search/'+this.sid;
+
+  //   fetch(u,{
+  //     method: 'GET',
+  //     headers: {'Content-Type': 'application/json' },
+  //     mode: 'cors'
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log('response from poll:');
+  //       console.log(data);
+  //     });
+  // }
+
+  // render(){
+  //   return (
+  //     <div className="results-panel">
+
+  //       {this.sid}
+
+  //     </div>
+  //   )
+  // }
+
+  return (
       <div className="results-panel">
 
-        {this.props.content}
+        {count}
 
       </div>
     )
-  }
 }
 
 export default ResultsPanel;
